6cba4a02742da7085ffb70f4098c1ea4bccb5b96
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-29 23:35:58 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,4 +38,12 @@ def using_uniq(array)
   array.uniq 
 end 
 
+def using_delete(array, string)
+  array.delete(string)
+end 
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
+
 
